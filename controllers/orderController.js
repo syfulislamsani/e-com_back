53d5cb39edff112ce182a58faec273b8cb146b48
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2025-02-19 10:41:33 +0600

    added user orders retrieval with error handling in orderController

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -49,7 +49,16 @@ const allOrders= async (req, res) => {
 
 // user order data for frontend
 const userOrders = async (req, res) => {
-    
+    try {
+        
+        const { userId } = req.body
+
+        const orders = await orderModel.find({userId})
+        res.json({success:true, orders})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
 }
 
 // update order status from admin
