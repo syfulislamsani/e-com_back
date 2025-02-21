17923edb1da1aabe90ad569631eb433d0d3d00d6
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2025-02-22 00:48:56 +0600

    added error handling for retrieving all orders and updating order status in orderController

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -45,6 +45,16 @@ const placeOrderRazorpay = async (req, res) => {
 // all orders for admin panel 
 const allOrders= async (req, res) => {
     
+    try {
+        
+        const orders = await orderModel.find({})
+        res.json({success:true, orders})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+
 }
 
 // user order data for frontend
@@ -63,7 +73,16 @@ const userOrders = async (req, res) => {
 
 // update order status from admin
 const updateStatus = async (req, res) => {
-    
+    try {
+
+        const {orderId, status} = req.body
+        await orderModel.findByIdAndUpdate(orderId, { status })
+        res.json({success:true, message:"Status Updated"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
 }
 
 export {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus }
